6573814971f63274784853c0e45ce5ebbec83d66
Krzysztof Hamerski <krzysztof.hamerski@gmail.com>
2021-05-06 14:09:34 +0200

    Added action.yml file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const core = require("@actions/core");
+
+try {
+  const url = core.getInput("sp_url");
+  console.log(`URL: ${url}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
